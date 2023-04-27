4a9e4c4a9cc4c851b4e90d02c4181a87ffb51d78
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2023-04-28 00:22:14 +0200

    .

---
+++ TEST/src/moy.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class moy {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("donner le nbr d'etudiants");
+        while (!scan.hasNextInt()) {
+            System.out.println("donner un entier");
+            scan.nextLine();
+        }
+
+        int x = scan.nextInt();
+        int g = 0;
+
+        for (int i = 0; i < x; i++) {
+            System.out.println("donner la note de l'etudiant num :"+(i+1));
+            while (!scan.hasNextInt()) {
+                System.out.println("donner un entier");
+                scan.nextLine();
+
+            }
+            int n = scan.nextInt();
+            while (n < 0 || n > 20) {
+                System.out.println("la note doit etre entre 0 et 20");
+                n=scan.nextInt();
+            }
+            //g=g+x;
+            g+=n;
+        }
+        double moy = (double) g / x;
+        System.out.println("la moy est = " + moy);
+    }
+}
