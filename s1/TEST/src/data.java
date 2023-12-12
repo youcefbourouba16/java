206f00d0d0796ef29ee1d808dd5d418356aa3e74
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2023-12-12 01:20:49 +0100

    .

---
+++ s1/TEST/src/data.java
@@ -0,0 +1,17 @@
+public class data {
+    public data(int number) {
+        this.number = number;
+    }
+
+    public data(String name, String password, int number, int id) {
+        this.name = name;
+        this.password = password;
+        this.number = number;
+        this.id = id;
+    }
+
+    String name,password;
+    int number,id;
+
+
+}
